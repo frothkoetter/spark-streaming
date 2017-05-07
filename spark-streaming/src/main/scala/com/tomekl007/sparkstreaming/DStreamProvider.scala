acf70dfae8c835a2c87d5d2db9d5c57234493b35
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-05-07 15:38:31 +0200

    implement logic of DStreamProvider

--- spark-streaming/src/main/scala/com/tomekl007/sparkstreaming/DStreamProvider.scala
+++ spark-streaming/src/main/scala/com/tomekl007/sparkstreaming/DStreamProvider.scala
@@ -5,21 +5,31 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.KafkaUtils
+import org.codehaus.jackson.map.ObjectMapper
 
 object DStreamProvider {
+  val objectMapper: ObjectMapper = new ObjectMapper()
 
   def providePageViews(ssc: StreamingContext): DStream[PageView] = {
-    KafkaUtils.createStream[Array[Byte], Array[Byte], DefaultDecoder, DefaultDecoder](
+    val properties = Map(
+      "bootstrap.servers" -> "broker1:9092,broker2:9092", //set your prod env configuration
+      "group.id" -> "bots-filtering",
+      "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+      "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer"
+    )
+
+    KafkaUtils.createStream[String, String, DefaultDecoder, DefaultDecoder](
       ssc,
-      Map("auto.offset.reset" -> "largest"),
+      properties,
       Map("page_views" -> 1),
       StorageLevel.MEMORY_ONLY
     ).map(deserializeToPageView)
+
+
   }
 
-  def deserializeToPageView(tuple: (Array[Byte], Array[Byte])): PageView = {
-    //deserialization logic, kafka format dependent
-    null
+  def deserializeToPageView(tuple: (String, String)): PageView = {
+    objectMapper.readValue(tuple._2, classOf[PageView]) //in prod it should be binary format, for example avro
   }
 
 }
