1be500000840d66f580fbc6c10479555f2d80638
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-06-09 19:25:18 +0200

    no need to register genericData

--- spark-streaming/src/main/scala/com/tomekl007/sparkstreaming/config/SparkApplication.scala
+++ spark-streaming/src/main/scala/com/tomekl007/sparkstreaming/config/SparkApplication.scala
@@ -1,6 +1,5 @@
 package com.tomekl007.sparkstreaming.config
 
-import org.apache.avro.generic.GenericData
 import org.apache.spark.{SparkConf, SparkContext}
 
 trait SparkApplication {
@@ -11,8 +10,6 @@ trait SparkApplication {
     val sparkConf = new SparkConf()
       .setAppName(sparkAppName)
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-
-    sparkConf.registerKryoClasses(Array(classOf[GenericData]))
 
     val sc = new SparkContext(conf(sparkConf))
 
